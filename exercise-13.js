bc4cdf89683b3e0b7500adf15b92937a1a51fe0c
unknown <kevintan203@gmail.com>
2019-11-28 11:09:37 +0700

    revisi exercise-13(comment:sedikit hardcode) dan exercise-15(comment:hardcode)

--- exercise-13.js
+++ exercise-13.js
@@ -1,36 +1,70 @@
 //Logic Challenge - Target Terdekat
 console.log('Logic Challenge - Target Terdekat')
-function targetTerdekat(arr) {
-	var x1 = arr.indexOf('x')
+function targetTerdekat(arr) {	
+	var x =[];
+	var o =[];
+	for (let i=0; i<arr.length; i++) {
+		if (arr[i]=='x') {
+			x.push(i);
+		}
+		else if (arr[i]=='o') {
+			o.push(i);
+		}
+	}
+	if (x.length == 0 || o.length==0) {
+		return 0
+	}
+	var nearest = arr.length;
+	for (let i=0; i<x.length; i++) {
+		for (let j=0; j<o.length; j++){
+			var compare = ''
+			if(x[i]>o[j]) {
+				compare = x[i]-o[j]
+				if (compare<nearest) {
+					nearest=compare
+				}
+			}
+			else {
+				compare = o[j]-x[i]
+				if (compare<nearest) {
+					nearest=compare
+				}
+			}
+		}
+	}
+	return nearest;
+}
+/*var x1 = arr.indexOf('x')
 	var o1 = arr.indexOf('o')
 	if (x1>=o1 && x1!=-1 && o1!=-1) { 
-	var a = x1-o1;
+		var a = x1-o1;
 	}
 	else if(x1<=o1 && x1!=-1 && o1!=-1) {
-	var a = o1-x1;
+		var a = o1-x1;
 	}
-	else {return 0}
+	else {return 0
+	}
 	var balik = [];
 	for (let i =arr.length; 0<i; i-- ) {
 		balik[balik.length] = arr[i-1];
 	}
+	
 	var x2 = balik.indexOf('x');
 	var o2 = balik.indexOf('o');
 	if (x2>=o2 && x2!=-1 && o2!=-1) {
-	var b = x2-o2;
+		var b = x2-o2;
 	}
 	else if(x2<=o2 && x2!=-1 && o2!=-1) {
-	var b = o2-x2;
+		var b = o2-x2;
 	}
-	else {return 0}
+	else {return 0
+	}
 	if (a>=b) {
 		return b
 	}
 	else if (a<=b ) {
 		return a
-	}
-}
-
+	}*/
 // TEST CASES
 console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
 console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
